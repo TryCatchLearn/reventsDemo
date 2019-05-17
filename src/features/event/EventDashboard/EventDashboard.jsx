0b7bbec9bd089209e82d868ba8c7f7fd9377c998
trycatchlearn <trycatchlearn@outlook.com>
2019-05-17 22:09:53 +0700

    End of section 4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const eventsFromDashboard = [
   {
@@ -61,10 +62,19 @@ class EventDashboard extends Component {
   };
 
   handleIsOpenToggle = () => {
-    this.setState(({isOpen}) => ({
+    this.setState(({ isOpen }) => ({
       isOpen: !isOpen
-    }))
-  }
+    }));
+  };
+
+  handleCreateEvent = newEvent => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    this.setState(({ events }) => ({
+      events: [...events, newEvent],
+      isOpen: false
+    }));
+  };
 
   render() {
     const { events, isOpen } = this.state;
@@ -74,8 +84,17 @@ class EventDashboard extends Component {
           <EventList events={events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button onClick={this.handleIsOpenToggle} positive content='Create Event' />
-          {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle} />}
+          <Button
+            onClick={this.handleIsOpenToggle}
+            positive
+            content='Create Event'
+          />
+          {isOpen && (
+            <EventForm
+              createEvent={this.handleCreateEvent}
+              cancelFormOpen={this.handleIsOpenToggle}
+            />
+          )}
         </Grid.Column>
       </Grid>
     );
