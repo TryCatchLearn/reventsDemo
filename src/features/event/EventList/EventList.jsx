b62bb606dd21fb9a8df768e36205734db61c2564
trycatchlearn <trycatchlearn@outlook.com>
2019-05-18 09:54:40 +0700

    End of section 5

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,16 +1,22 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
 import EventListItem from './EventListItem';
 
 class EventList extends Component {
   render() {
+    const { events, selectEvent, deleteEvent } = this.props;
     return (
       <Fragment>
-        {this.props.events.map(event => (
-          <EventListItem key={event.id} event={event}/>
+        {events.map(event => (
+          <EventListItem
+            key={event.id}
+            event={event}
+            selectEvent={selectEvent}
+            deleteEvent={deleteEvent}
+          />
         ))}
       </Fragment>
-    )
+    );
   }
 }
 
-export default EventList
+export default EventList;
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -4,22 +4,16 @@ import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
+    const { event, selectEvent, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
           <Item.Group>
             <Item>
-              <Item.Image
-                size='tiny'
-                circular
-                src={event.hostPhotoURL}
-              />
+              <Item.Image size='tiny' circular src={event.hostPhotoURL} />
               <Item.Content>
                 <Item.Header>{event.title}</Item.Header>
-                <Item.Description>
-                  Hosted by {event.hostedBy}
-                </Item.Description>
+                <Item.Description>Hosted by {event.hostedBy}</Item.Description>
               </Item.Content>
             </Item>
           </Item.Group>
@@ -32,14 +26,28 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attendees && event.attendees.map(attendee => (
-              <EventListAttendee key={attendee.id} attendee={attendee} />
-            ))}
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
-          <Button as='a' color='teal' floated='right' content='View' />
+          <Button
+            onClick={() => deleteEvent(event.id)}
+            as='a'
+            color='red'
+            floated='right'
+            content='Delete'
+          />
+          <Button
+            onClick={() => selectEvent(event)}
+            as='a'
+            color='teal'
+            floated='right'
+            content='View'
+          />
         </Segment>
       </Segment.Group>
     );
