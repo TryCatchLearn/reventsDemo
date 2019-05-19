69fed8092024ac24140d924616952f733f8c166d
trycatchlearn <trycatchlearn@outlook.com>
2019-05-19 14:08:20 +0700

    End of section 7

---
+++ src/features/testarea/testActions.js
@@ -0,0 +1,14 @@
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+
+export const incrementCounter = () => {
+    return {
+        type: INCREMENT_COUNTER
+    }
+}
+
+export const decrementCounter = () => {
+    return {
+        type: DECREMENT_COUNTER
+    }
+}
+
