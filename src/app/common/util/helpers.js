3fa4d14a119c0a2fcd36117751921c93b5c7aefa
trycatchlearn <trycatchlearn@outlook.com>
2019-05-26 13:29:19 +0700

    End of section 21

--- src/app/common/util/helpers.js
+++ src/app/common/util/helpers.js
@@ -21,3 +21,14 @@ export const createNewEvent = (user, photoURL, event, firestore) => {
         }
     }
 }
+
+export const createDataTree = dataset => {
+    let hashTable = Object.create(null);
+    dataset.forEach(a => hashTable[a.id] = {...a, childNodes: []});
+    let dataTree = [];
+    dataset.forEach(a => {
+        if (a.parentId) hashTable[a.parentId].childNodes.push(hashTable[a.id]);
+        else dataTree.push(hashTable[a.id])
+    });
+    return dataTree
+};
